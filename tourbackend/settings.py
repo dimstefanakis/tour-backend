52d53141c45ea6b8bb242a714ee2a56701059d07
Dimitris <jimstef@outlook.com>
2023-01-09 02:41:34 +0200

    changed settings

--- tourbackend/settings.py
+++ tourbackend/settings.py
@@ -28,13 +28,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
     'tour-backend-edwu.onrender.com',
     'tour-frontend.onrender.com'
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
